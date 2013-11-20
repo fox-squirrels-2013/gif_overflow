0c20f75007fddb403ab504f3d8c9ddcc878542f9
Apprentice <apprentice@dbc10.local>
2013-11-20 14:50:29 -0800

    first_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GifOverflow::Application.config.secret_token = 'd46bd64dd66625b227d14f38cccd2c524e58bd93f5a95fc7bf1a4148709e496a6f4cf4e148e1ba80df0de61ef3d0d84029e73da42b4b24ca8bacb1b8f7a13bea'
