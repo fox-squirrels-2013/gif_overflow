1fb8edfc9d2f154938babb046dec9163d8a7bd00
Louie Perez <louie.perez3@gmail.com>
2013-11-20 23:14:25 -0800

    Added shoulda matchers and factory girl.  Updated all tests

--- spec/controller/gifs/gifs_controller_spec.rb
+++ spec/controller/gifs/gifs_controller_spec.rb
@@ -1,1 +1,35 @@
 require "spec_helper"
+
+describe GifsController, :type => :controller do
+ 
+  describe '#index' do
+    before(:each) do
+      Gif.create(title: 'Hello', link: 'Goodbye')
+      get :index
+    end
+    it { should respond_with(:success) }
+    it { should render_template(:index) }
+  end
+
+  describe '#show' do
+    before(:each) do
+      @test = Gif.create(title: 'Hello', link: 'Goodbye')
+      get :show, :id => @test.id
+    end
+    it { should respond_with(:success) }
+    it { should render_template(:show) }
+  end
+
+  describe '#create' do
+    before(:each) do
+      post :create, title: "Hello", link: "Test"
+    end
+    it { should respond_with(302) }
+  end
+
+  describe 'Routes' do
+    it { should route(:get, '/gifs/1').to(:action => :show, :id => 1)}
+    it { should route(:get, '/gifs').to('/') }
+  end
+
+end
--- spec/model/gifs/gifs_model_spec.rb
+++ spec/model/gifs/gifs_model_spec.rb
@@ -1,9 +1,7 @@
   require 'spec_helper'
 
-  describe "Upvoting" do
-    it "increases the Vote Count by one" do
-      gif_to_upvote = Gif.create(title: "Hello", link: "kitty")
-      gif_to_upvote.upvote
-      expect(gif_to_upvote.vote_count).to eq(1)
-    end
+  describe Gif do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:link) }
+    it { should have_many(:gifcomments) }
   end
