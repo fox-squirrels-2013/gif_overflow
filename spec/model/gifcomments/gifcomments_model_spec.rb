50d09f652533b3642a9b4c7634f59074fcc0f73c
Louie Perez <louie.perez3@gmail.com>
2013-11-20 17:59:02 -0800

    Ad

--- spec/model/gifcomments/gifcomments_model_spec.rb
+++ spec/model/gifcomments/gifcomments_model_spec.rb
@@ -3,10 +3,8 @@ require "spec_helper"
 describe "Gifcomment" do
   
   it "should not create without a link" do
-    p "-"*50
-    p Gif.create()
+    p Gifcomment.create()
     expect(Gif.count).to eq(0)
-    p "-"*50
   end
 
 end
