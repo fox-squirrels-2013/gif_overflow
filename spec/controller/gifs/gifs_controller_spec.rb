e41b3bf67541c5ca0faf5210d68b22a2f41efd48
Louie Perez <louie.perez3@gmail.com>
2013-11-20 16:22:51 -0800

    Corrected Test to detect change

--- spec/controller/gifs/gifs_controller_spec.rb
+++ spec/controller/gifs/gifs_controller_spec.rb
@@ -3,6 +3,7 @@
   describe "Upvoting" do
     it "increases the Vote Count by one" do
       gif_to_upvote = Gif.create()
-      expect(gif_to_upvote.upvote).to change(gif_to_upvote.vote_count).from(0).to(1)
+      gif_to_upvote.upvote
+      expect(gif_to_upvote.vote_count).to eq(1)
     end
   end
