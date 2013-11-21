eb32fcd7c7fce827854a2ce7001ad941430f08f6
Louie Perez <louie.perez3@gmail.com>
2013-11-20 16:12:33 -0800

    Clarified variable names in test

--- spec/controller/gifs/gifs_controller_spec.rb
+++ spec/controller/gifs/gifs_controller_spec.rb
@@ -1,6 +1,6 @@
   describe "Upvoting" do
     it "increases the Vote Count by one" do
-      upvote = Gifs.create()
-      expect(Gifs.upvote).to change(upvote.vote_count).from(0).to(1)
+      gif_to_upvote = Gif.create()
+      expect(gif_to_upvote.upvote).to change(gif_to_upvote.vote_count).from(0).to(1)
     end
   end
