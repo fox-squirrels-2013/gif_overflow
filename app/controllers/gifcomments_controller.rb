db0b3e7f94f24533a90c726fd5c03732b94c631b
Louie Perez <louie.perez3@gmail.com>
2013-11-20 18:28:16 -0800

    Modified Create Route

--- app/controllers/gifcomments_controller.rb
+++ app/controllers/gifcomments_controller.rb
@@ -3,6 +3,8 @@ class GifcommentsController < ApplicationController
   end
 
   def create
+    @gifcomment = Gifcomment.create(gif_id: params[:gif_id], link: params[:link])
+    redirect_to root
   end
 
   def show
