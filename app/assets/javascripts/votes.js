7c35379992df32c525b6adfcced46a0020b00c05
Apprentice <apprentice@dbc-11.local>
2013-11-21 17:52:42 -0800

    finished with the ajaxification

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,19 +1,22 @@
-$(document).ready(function() {
+$(document).ready(
 
-function onSuccess(e, response, textStatus) {
+  initialize
+
+);
+
+function initialize() {
+
+function ajaxvote(e, response, textStatus) {
   $(this).closest('li').find('span').remove()
   $(this).closest('li').find('a').after('<span>'+response.voteCount+'</span>')
 }
 
-function Succeeded(e, response, textStatus) {
-  debugger
+function ajaxgif(e, response, textStatus) {
+  $('ul.index_gifs').append(response.gif_add)
 }
 
+$('.index_gifs').on('ajax:success', 'li', ajaxvote)
+$('#new_gif').on('ajax:success', ajaxgif)
 
 
-$('.button_to').on('ajax:success', onSuccess)
-
-$('#new_gif').on('ajax:success', Succeeded)
-
-
-});
+}
