4a92aad5602645ec56c10634353b67cdbdee7ce4
Apprentice <apprentice@dbc10.local>
2013-11-21 20:53:50 -0800

    Deleted Extraneous BCrypt Stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
-require 'bcrypt'
-
 class User < ActiveRecord::Base
   attr_accessible :username, :email, :password_digest
 
@@ -8,17 +6,5 @@ class User < ActiveRecord::Base
 
   validates :email, format: { :with => /[a-z0-9]+@[a-z0-9]{2,}\.[a-z]{2,}/i,
     message: "Invalid email address. Please try again." }
-
-  include BCrypt
-
-  def password
-    @password ||= Password.new(password_digest)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_digest = @password
-  end
-
 
  end
