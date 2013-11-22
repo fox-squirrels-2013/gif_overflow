01e48f0e6fdce47280c43f0adb3616fe1fa6466a
Apprentice <apprentice@dbc10.local>
2013-11-21 20:55:01 -0800

    Corrected password_digest field name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :username, :email, :password_digest
+  attr_accessible :username, :email, :password
 
-  validates :username, :email, :password_digest, presence: true
+  validates :username, :email, :password, presence: true
   validates :username, :email, uniqueness: true
 
   validates :email, format: { :with => /[a-z0-9]+@[a-z0-9]{2,}\.[a-z]{2,}/i,
     message: "Invalid email address. Please try again." }
+
+  has_secure_password
 
  end
