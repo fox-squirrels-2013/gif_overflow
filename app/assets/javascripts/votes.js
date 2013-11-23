0e09c2b725f1d680191a75a7ed7be4bec6097d87
Apprentice <apprentice@dbc-11.local>
2013-11-22 16:04:13 -0800

    dynamically changes vote in the data-votecount

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,29 +1,31 @@
 $(document).ready(
 
-  initialize
+  bindListeners
 
 );
 
-function initialize() {
+function bindListeners() {
+
+  $('.index_gifs').on('ajax:success', 'li', ajaxvote) //placed event listener on class ul and its children
+  $('#new_gif').on('ajax:success', ajaxgif)
+  $('.new_gifcomment').on('ajax:success', gifresponse) // placed event listener on form
+
+}
 
 function ajaxvote(e, response, textStatus) {
+
   $(this).closest('li').find('span').remove() //checks to see if there was already a span tag, if so, deletes it.
   $(this).closest('li').find('a').after('<span>'+response.voteCount+'</span>')
+
+  var num = response.voteCount
+  $(this).closest('li').attr('data-votecount', num) //change data-votecount dynamically with every click
 }
 
 function ajaxgif(e, response, textStatus) {
-  $('ul.index_gifs').append(response.gif_add) //the key gif_add has a value which is a partial(check controller)
+  $('ul.index_gifs').prepend(response.gif_add) //the key gif_add has a value which is a partial(check controller)
 }
 
 function gifresponse(e, response, textStatus) {
   $('ul.respond_gif').append('<img src='+response.link+'>')
 }
 
-$('.index_gifs').on('ajax:success', 'li', ajaxvote) //placed event listener on class ul and
-                                                    //its children
-
-$('#new_gif').on('ajax:success', ajaxgif)
-
-$('.new_gifcomment').on('ajax:success', gifresponse) // placed event listener on form
-
-}
