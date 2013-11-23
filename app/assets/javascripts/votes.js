adaa5de762710b721edb48e06b831e552e8d1c57
Apprentice <apprentice@dbc-11.local>
2013-11-22 20:04:23 -0800

    work that was done before myles

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -10,14 +10,6 @@ function bindListeners() {
   $('.new_gifcomment').on('ajax:success', gifresponse) // placed event listener on form
 }
 
-function ajaxvote(e, response, textStatus) {
-  $(this).closest('li').find('span').remove() //checks to see if there was already a span tag, if so, deletes it.
-  $(this).closest('li').find('a').after('<span>'+response.voteCount+'</span>')
-
-  var num = response.voteCount
-  $(this).closest('li').attr('data-votecount', num) //change data-votecount dynamically with every click
-}
-
 function ajaxgif(e, response, textStatus) {
   $('ul.index_gifs').prepend(response.gif_add) //the key gif_add has a value which is a partial(check controller)
 }
@@ -26,5 +18,30 @@ function gifresponse(e, response, textStatus) {
   $('ul.respond_gif').append('<img src='+response.link+'>')
 }
 
+function ajaxvote(e, response, textStatus) {
+  $(this).closest('li').find('span').remove() //checks to see if there was already a span tag, if so, deletes it.
+  $(this).closest('li').find('a').after('<span>'+response.voteCount+'</span>')
+
+  var num = response.voteCount
+  $(this).closest('li').attr('data-votecount', num) //change data-votecount dynamically with every click
 
 
+}
+
+
+// function moveUp() {
+//   $before = $(this).prev();
+//   this.insertBefore($before);
+// }
+
+// var n = response.voteCount;
+
+// while (n < checkUpvotes) {
+//   moveUp
+// }
+
+
+// function checkUpvotes() {
+//   before = $(this).prev().attr('data-votecount')
+//   return before
+// }
--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,12 +1,13 @@
 class GifsController < ApplicationController
 
   def index
-    @gifs = Gif.all
+    @gifs = _all_gifs
     @gif = Gif.new
   end
 
   def create
     gif = Gif.create(params[:gif])
+    gifs = _all_gifs
     render json: { gif_add: render_to_string(partial: "postgif", :locals => {:gif => gif}) }
     #render_to_string method is a way of rendering a partial inside of a json
   end
@@ -16,6 +17,11 @@ class GifsController < ApplicationController
     @gifcomment = Gifcomment.new
     @gifshow = Gifcomment.where("gif_id = #{@gif.id}")
   end
+
+  def _all_gifs
+    Gif.all.sort_by { |gif| [gif.votes, gif.created_at] }.reverse
+  end
+
 
 end
 
