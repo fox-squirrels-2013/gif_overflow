6b64c9ce85ef0cad736be4042184ef951559e144
Apprentice <apprentice@dbc-11.local>
2013-11-23 23:40:57 -0800

    finished ajaxifying whole thing

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -7,13 +7,19 @@ $(document).ready(
 function bindListeners() {
 $('#new_gif').on('ajax:success', ajaxgif)
 $('div#rendergif').on('ajax:success', 'ul', ajaxvote)
+$('.new_gifcomment').on('ajax:success', ajaxgifcomment)
 }
 
 
 function ajaxgif(e, response, textStatus) {
   $('div#rendergif').html(response.gif_add)
 }
+
 function ajaxvote(e, response, textStatus) {
   $('div#rendergif').html(response.vote_add)
 }
 
+function ajaxgifcomment(e, response, textStatus) {
+
+  $('div#rendergifcomments').html(response.gifcomment_add)
+}
--- app/controllers/gifcomments_controller.rb
+++ app/controllers/gifcomments_controller.rb
@@ -2,7 +2,9 @@ class GifcommentsController < ApplicationController
 
   def create
     @gifcomment = Gifcomment.create(gif_id: params[:gif_id], link: params[:gifcomment][:link])
-    render json: @gifcomment
+    @gifcomments = Gifcomment.all
+
+    render json: { gifcomment_add: render_to_string(partial: "postgifcomments", :locals => {:gifcomments => @gifcomments}) }
   end
 
 end
